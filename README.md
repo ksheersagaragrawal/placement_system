928b99edfc0997d1b9d206bac7e88f089bf4e191
Sanskar Sharma <sanskarfc@gmail.com>
2023-03-16 20:29:56 +0530

    dummy data for student added and checked

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # placement_system 
 
-###server.js file 
+### server.js file 
 i have added the server.js file for frontend people to test their api endpoints and get preview of web pages 
--- server.js
+++ server.js
@@ -36,12 +36,23 @@ const opportunities = [
     address_line_3: "Palo Alto, CA 94303",
     company_id: 3
   }
-];  
+];   
+
+const students = [
+  { student_id: 1, student_first_name: "John", student_middle_name: "A", student_last_name: "Doe", dept: "CSE", CPI: "9.5", active_backlogs: "no", gender: "male", study_year: 4 },
+  { student_id: 2, student_first_name: "Jane", student_middle_name: "B", student_last_name: "Doe", dept: "EE", CPI: "8.2", active_backlogs: "yes", gender: "female", study_year: 3 },
+  { student_id: 3, student_first_name: "Bob", student_middle_name: "C", student_last_name: "Smith", dept: "ME", CPI: "7.8", active_backlogs: "yes", gender: "male", study_year: 2 }
+];
+
 
 // Define a route handler for GET requests on "/api/poc/opportunities"
 app.get('/api/poc/opportunities', (req, res) => {
   res.send(opportunities);
-});  
+});   
+
+app.get('/api/poc/opportunities/student', (req, res) => {
+  res.send(students);
+});
 
 app.listen(5500, () => {
   console.log('Server listening on port 5500');
