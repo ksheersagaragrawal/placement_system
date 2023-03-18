14cdf9dc8a4e52385ca6d95a5ab21dbe6679b5ff
Dhyey Thummar <dhyeythummar8@gmail.com>
2023-03-18 16:19:29 +0530

    Added POST request handling functionality to backend

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ from flask_mysqldb import MySQL
 import yaml
 from enum import Enum
 import json
- 
+
+
 class Occupation(Enum):
     STUDENT = 1
     CDS_EMPLOYEE = 2
     COMPANY_POC = 3
+
+
+# Temporary hain, until we get the student id by session or login shit
+global student_id
+student_id = 3
 
 USER = Occupation.STUDENT
 
@@ -22,6 +28,7 @@ app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -38,13 +45,15 @@ def index():
     return render_template('index.html')
 
 ##### for student get requests#####
+
+
 @app.route('/opportunities')
 def get_opportunities():
     # we need to check if the user is a student or not
-    if(USER != Occupation.STUDENT):
+    if (USER != Occupation.STUDENT):
         return jsonify({"error": "Invalid Access"}), 404
-    
-    # get the student id and status from the request, request will be like: /opportunities?student_id=1&status=applied 
+
+    # get the student id and status from the request, request will be like: /opportunities?student_id=1&status=applied
     student_id = request.args.get('student_id')
     status = request.args.get('status')
 
@@ -54,8 +63,9 @@ def get_opportunities():
         cur = mysql.connection.cursor()
 
         # we execute the query
-        resultValue = cur.execute("SELECT * FROM opportunity INNER JOIN requirements ON opportunity.opp_id = requirements.opp_id;")
-        
+        resultValue = cur.execute(
+            "SELECT * FROM opportunity INNER JOIN requirements ON opportunity.opp_id = requirements.opp_id;")
+
         # get the field names(columns) of the query
         field_names = [i[0] for i in cur.description]
 
@@ -64,7 +74,6 @@ def get_opportunities():
 
             # fetch all the results as a list of tuples and store it in opportunities
             opportunities = cur.fetchall()
-            
 
             # below code segment is to convert the list of tuples to a list of dictionaries
             final_opportunities = []
@@ -73,10 +82,9 @@ def get_opportunities():
                 for i in range(len(cur.description)):
                     dict[field_names[i]] = opportunities[j][i]
                 final_opportunities.append(dict)
-            
+
             # return the list of dictionaries as json response
             return jsonify(final_opportunities)
-        
 
     # if any both queried values are present, return the opportunities with the queried status
     elif student_id is not None and status is not None:
@@ -86,7 +94,7 @@ def get_opportunities():
             query = "select * from selection_procedure where opp_id in (select opp_id from app_opp where student_id = %s)"
             resultValue = cur.execute(query, (student_id,))
             field_names = [i[0] for i in cur.description]
-            if resultValue>0:
+            if resultValue > 0:
                 opportunities = cur.fetchall()
                 final_opportunities = []
                 for j in range(len(opportunities)):
@@ -99,7 +107,7 @@ def get_opportunities():
             query = "select * from opportunity where opp_id in (select opp_id from app_opp where student_id = %s)"
             resultValue = cur.execute(query, (student_id,))
             field_names = [i[0] for i in cur.description]
-            if resultValue>0:
+            if resultValue > 0:
                 opportunities = cur.fetchall()
                 final_opportunities = []
                 for j in range(len(opportunities)):
@@ -111,35 +119,107 @@ def get_opportunities():
         else:
             return jsonify({"error": "invalid status"}), 404
 
+
+@app.route('/api/cds/opportunity', methods=['POST'])
+def add_new_opportunity():
+    if USER != Occupation.CDS_EMPLOYEE:
+        return jsonify({"error": "Invalid Access"}), 404
+    print(request.get_json())
+    opportunity = request.get_json()
+    # Columns in opportunity table
+    # opportunity (opp_type, opp_title, address_line_1, address_line_2, address_line_3, company_id)
+    opp_type = opportunity['opp_type']
+    opp_title = opportunity['opp_title']
+    address_line_1 = opportunity['address_line_1']
+    address_line_2 = opportunity['address_line_2']
+    address_line_3 = opportunity['address_line_3']
+    company_id = opportunity['company_id']
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO opportunity(opp_type, opp_title, address_line_1, address_line_2, address_line_3, company_id) VALUES(%s, %s, %s, %s, %s, %s)",
+                (opp_type, opp_title, address_line_1, address_line_2, address_line_3, company_id))
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({"message": "Opportunity added successfully"}), 200
+
+
+@app.route('/api/cds/opportunity_delete', methods=['POST'])
+def delete_opportunity():
+    if USER != Occupation.CDS_EMPLOYEE:
+        return jsonify({"error": "Invalid Access"}), 404
+    opportunity = request.get_json()
+    opp_id = opportunity['opp_id']
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM opportunity WHERE opp_id = %s", (opp_id,))
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({"message": "Opportunity deleted successfully"}), 200
+
+
+@app.route('/api/student/resume', methods=['POST'])
+def upload_resume():
+    if USER != Occupation.STUDENT:
+        return jsonify({"error": "Invalid Access"}), 404
+    resume = request.get_json()
+    # Format resume (resume_id, resume_file VARCHAR(255) CHECK (resume_file REGEXP '^(http|https)://.+'), resume_file_name);
+    resume_file = resume['resume_file']
+    resume_file_name = resume['resume_file_name']
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO resume(resume_file, resume_file_name) VALUES(%s, %s)",
+                (resume_file, resume_file_name))
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({"message": "Resume uploaded successfully"}), 200
+
+
+@app.route('/api/student/image', methods=['POST'])
+def upload_image():
+    if USER != Occupation.STUDENT:
+        return jsonify({"error": "Invalid Access"}), 404
+    image = request.get_json()
+    # Format student (studentid, student_image)
+    id = student_id
+    student_image = image['student_image']
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "UPDATE student SET student_image = %s WHERE student_id = %s", (student_image, id))
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({"message": "Image uploaded successfully"}), 200
+
+
 @app.route('/opportunity')
 def get_opportunity_by_id():
-    if(USER != Occupation.STUDENT):
+    if (USER != Occupation.STUDENT):
         return jsonify({"error": "Invalid Accesss"}), 404
     opp_id = request.args.get('opp_id')
     cur = mysql.connection.cursor()
-    resultValue = cur.execute("select * from opportunity where opp_id = %s", (opp_id,))
+    resultValue = cur.execute(
+        "select * from opportunity where opp_id = %s", (opp_id,))
     field_names = [i[0] for i in cur.description]
     if resultValue > 0:
         opportunity_desc = cur.fetchone()
         dict = {}
         for i in range(len(cur.description)):
             dict[field_names[i]] = opportunity_desc[i]
-        return jsonify(dict) 
-    
+        return jsonify(dict)
+
+
 @app.route('/student')
 def get_student_by_id():
-    if(USER != Occupation.STUDENT):
+    if (USER != Occupation.STUDENT):
         return jsonify({"error": "Invalid Access"}), 404
     stud_id = request.args.get('student_id')
     cur = mysql.connection.cursor()
-    resultValue = cur.execute("select * from student where student_id = %s", (stud_id,))
+    resultValue = cur.execute(
+        "select * from student where student_id = %s", (stud_id,))
     field_names = [i[0] for i in cur.description]
     if resultValue > 0:
         student_desc = cur.fetchone()
         dict = {}
         for i in range(len(cur.description)):
             dict[field_names[i]] = student_desc[i]
-        return jsonify(dict) 
+        return jsonify(dict)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
